cb1a82d03a1ae2b3951c50c5ccdf1e899e2b53cf
szyman <szyman.marek@gmail.com>
2013-09-16 14:49:46 +0200

    implemented CRUD for posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsSample::Application.config.secret_key_base = '32b28a15bf8eaf82b4e6e9883434652b8f0d3bedc9b4c867cfd6d0613963fe333b72035c5233a67b6e820b323caf88dc5ae428e9cd09dab4b97f54f1731a52e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsSample::Application.config.session_store :cookie_store, key: '_railsSample_session'
